e55fb8e80faa8288cb846a8a13bde404e780548b
nealgarcia <nathnlgarcia@gmail.com>
2022-04-09 22:27:47 -0500

    Panel header styling

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { Offcanvas } from "react-bootstrap";
 import "./Panel.css";
-import CloseIcon from '@material-ui/icons/Close';
+import CloseIcon from "@material-ui/icons/Close";
 
 function Panel({ showPanel, handleClose, teamData }) {
-
   return (
     <Offcanvas placement="end" show={showPanel} onHide={handleClose}>
-      <Offcanvas.Title>
-          {teamData.name}
-          <CloseIcon
-            className = "button"
-            onClick = {handleClose}
-          />
-          </Offcanvas.Title>
+      <Offcanvas.Title className = "panelHeader">
+        <h3 className = "teamName">{teamData.name}</h3>
+        <CloseIcon className="button" onClick={handleClose} />
+      </Offcanvas.Title>
       <Offcanvas.Body>
         Some text as placeholder. In real life you can have the elements you
         have chosen. Like, text, images, lists, etc.
