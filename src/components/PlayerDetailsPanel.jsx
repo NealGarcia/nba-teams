e641824d361cf0456e305504db4180f3f681d408
nealgarcia <nathnlgarcia@gmail.com>
2022-04-12 16:33:13 -0500

    Display all player data to player modal

--- src/components/PlayerDetailsPanel.jsx
+++ src/components/PlayerDetailsPanel.jsx
@@ -1,30 +1,51 @@
-import React from 'react';
-import { Modal } from "react-bootstrap";
+import React from "react";
+import { Modal, Container, Row, Col } from "react-bootstrap";
 import CloseIcon from "@material-ui/icons/Close";
-import { CircularProgress } from "@material-ui/core";
 
 import "./PlayerDetailsPanel.css";
 
-
-function PlayerDetailsPanel({ setShowDetailsPanel, showDetailsPanel, player, playerDetailData }) {
-    return (
-        <Modal
-            show = {showDetailsPanel}
-            onHide = {() => {
-                setShowDetailsPanel(false)
-            }}
-        >
-            <Modal.Header>
-                <Modal.Title className = "modalTitle">{playerDetailData.first_name} {playerDetailData.last_name}</Modal.Title>
-                <CloseIcon
+function PlayerDetailsPanel({ setShowDetailsPanel, showDetailsPanel, player, playerDetailData, }) {
+  return (
+    <Modal
+      show={showDetailsPanel}
+      onHide={() => {
+        setShowDetailsPanel(false);
+      }}
+    >
+      <Modal.Header>
+        <Modal.Title className="modalTitle">
+          {playerDetailData.first_name} {playerDetailData.last_name}
+        </Modal.Title>
+        <CloseIcon
           className="closeButton"
           onClick={() => {
-            setShowDetailsPanel(false)
+            setShowDetailsPanel(false);
           }}
         />
-            </Modal.Header>
-        </Modal>
-    )
+      </Modal.Header>
+      <Modal.Body>
+        <Container>
+            <Row>
+                <Col>Team: {playerDetailData.team.full_name}</Col>
+            </Row>
+            <Row>
+                <Col>Position: {playerDetailData.position}</Col>
+            </Row>
+            <Row>
+                <Col>Height: {playerDetailData.height_feet} feet, {playerDetailData.height_inches} inches</Col>
+            </Row>
+            <Row>
+                <Col>Weight: {playerDetailData.weight_pounds} pounds</Col>
+            </Row>
+        </Container>
+
+      </Modal.Body>
+
+
+
+    
+    </Modal>
+  );
 }
 
 export default PlayerDetailsPanel;
